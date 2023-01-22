656773cf0fbaf519152457fd22d48f17f79a76ef
Ramanand Thakur <ramanandt35@gmail.com>
2023-01-22 14:19:50 +0530

    Blog and Footer added

--- src/containers/possibility/Possibility.jsx
+++ src/containers/possibility/Possibility.jsx
@@ -3,7 +3,7 @@ import './possibility.css';
 import possibilityImage from '../../assets/possibility.png'
 const Possibility = () => {
   return (
-    <div className="gpt3__possibility section__padding" >
+    <div className="gpt3__possibility section__padding" id='possibility'>
       <div className="gpt3__possibility-image">
         <img src={possibilityImage} alt="possibility" />
       </div>
